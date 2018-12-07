679f80aa8974d33bd1760f794e3d4cb89761981c
chenhaiwen <imfukua@sina.com>
2018-12-07 16:46:07 +0800

    添加抽奖工具

---
+++ src/main/java/com/albert/dragonbones/DrawUtil.java
@@ -0,0 +1,47 @@
+package com.albert.dragonbones;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 抽奖
+ */
+public class DrawUtil {
+	/**
+     * 抽奖
+     *
+     * @param orignalRates
+     *            原始的概率列表，保证顺序和实际物品对应
+     * @return
+     *         物品的索引
+     */
+    public static int lottery(List<Double> orignalRates) {
+        if (orignalRates == null || orignalRates.isEmpty()) {
+            return -1;
+        }
+
+        int size = orignalRates.size();
+
+        // 计算总概率
+        double sumRate = 0d;
+        for (double rate : orignalRates) {
+            sumRate += rate;
+        }
+
+        // 计算每个物品在总概率的比重
+        List<Double> sortOrignalRates = new ArrayList<Double>(size);
+        Double tempSumRate = 0d;
+        for (double rate : orignalRates) {
+            tempSumRate += rate;
+            sortOrignalRates.add(tempSumRate / sumRate);
+        }
+
+        // 根据随机数来获取抽取到的物品索引
+        double nextDouble = Math.random();
+        sortOrignalRates.add(nextDouble);
+        Collections.sort(sortOrignalRates);
+
+        return sortOrignalRates.indexOf(nextDouble);
+    }
+}
